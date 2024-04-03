0b242d6fc2bec6d9383ab4728ba19cbb55d62cac
pascal <pascal.nicintije@student.moringaschool.com>
2024-04-03 16:30:16 +0300

    updates index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const descriptionInput = document.getElementById("new-task-description");
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const lisItem = document.createElement("li");
+    lisItem.innerText = descriptionInput.value;
+    const remove_btn = document.createElement("button");
+    remove_btn.innerText = "remove";
+    remove_btn.addEventListener("click", (event) => {
+      event.target.parentNode.remove(list_item);
+    });
+    lisItem.appendChild(remove_btn);
+    taskList.appendChild(list_item);
+  });
+});
 });
